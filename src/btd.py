31b04c90a0859aa6d20cf299f6fa230d842760c8
Nick <nickxz149@gmail.com>
2019-09-21 15:37:06 -0400

    added comments for future edits

--- src/btd.py
+++ src/btd.py
@@ -1,16 +1,22 @@
 import math
 
 def binaryToDecimal(binary):
+    # checks if its a int
     if (binary > 0):
+        # used for return statement
         bi = binary
+        # reverses the binary 
         strbi = str(binary)
         strbi = strbi[::-1]
         n = 0
         p = 0
+        # goes through all of the 1s and 0s
         for x in strbi:
+            # code for 1
             if (int(x) == 1):
                 n = n + (2 ** p)
                 p += 1
+            # code for 0
             elif (int(x) == 0):
                 p += 1
         return "\nNumber: " + str(n) + "\nBinary: " + str(bi) + "\n"
--- src/dtb.py
+++ src/dtb.py
@@ -1,13 +1,19 @@
 import math
 
 def decimalToBinary(num):
+    # checks if int
     if (num > 0):
+        # used for return
         nu = num
         b = ""
+        # goes through the number
         for x in range(num):
+            # takes the reminder
             t = num % 2
+            # divdes by 2
             num /= 2
             b = b + str(math.floor(t))
+        # strips the other 0s
         nb = b.rstrip("0")
         return "\nNumber: " + str(nu) + "\nBinary: " + str(nb[::-1]) + "\n"
     else:
